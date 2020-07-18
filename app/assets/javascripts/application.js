79c8f8329864ceaef5f2558139ddcf5cf5d9be43
tsuny221 <kingoftsuny@gmail.com>
2020-07-18 07:47:43 +0000

    [Update] main images

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,14 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(window).scroll(function () {
+  $(".fadein").each(function () {
+    var elemPos = $(this).offset().top,
+      scroll = $(window).scrollTop(),
+      windowHeight = $(window).height();
+    if (scroll > elemPos - windowHeight + 100) {
+      $(this).addClass("scrollin");
+    }
+  });
+});
