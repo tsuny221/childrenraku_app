c5a7960619b13288d820c46d0f401417d9ef174e
tsuny221 <kingoftsuny@gmail.com>
2020-07-22 02:42:29 +0000

    [Update] favicon

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :Host => 'http://childrenraku.work/' }
+
+  config.action_mailer.default_url_options = { :host => 'http://childrenraku.work/' }
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
