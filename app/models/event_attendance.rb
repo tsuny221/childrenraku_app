979daab650ad3876899c2c398e83ab2b944ff6d2
tsuny221 <kingoftsuny@gmail.com>
2020-07-25 06:48:27 +0000

    [Update] top_page model

--- app/models/event_attendance.rb
+++ app/models/event_attendance.rb
@@ -1,7 +1,9 @@
 class EventAttendance < ApplicationRecord
   belongs_to :event, optional: true
+  belongs_to :family, optional: true
   has_many :event_participants
   accepts_nested_attributes_for :event_participants, allow_destroy: true
+
   enum attendance: { 出席: 1, 欠席: 2, 保留: 3 }
   validates :family_id, :uniqueness => { :scope => :event_id }
   validates :attendance, presence: true
--- app/models/family.rb
+++ app/models/family.rb
@@ -2,4 +2,5 @@ class Family < ApplicationRecord
   belongs_to :room, optional: true
   has_many :users, dependent: :destroy
   has_many :children, dependent: :destroy
+  has_many :event_attendances, dependent: :destroy
 end
